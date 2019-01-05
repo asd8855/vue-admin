7e84a9b7109f963ef1c53050ed1edbf740b2e3be
libo <asd13148866@sina.com>
2019-01-05 12:08:50 +0800

    modify

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -11,7 +11,7 @@
           <el-dropdown-item>新增</el-dropdown-item>
           <el-dropdown-item>删除</el-dropdown-item>
         </el-dropdown-menu>
-      </el-dropdown>
+      </el-dropdown
       <span>王小虎</span>
     </el-header>
     <el-container>
@@ -38,20 +38,9 @@
             </el-submenu>
           </el-submenu>
           <el-submenu index="2">
-            <template slot="title">
-              <i class="el-icon-menu"></i>导航二
+           <template slot="title">
+              <i class="el-icon-setting"></i>导航二
             </template>
-            <el-menu-item-group>
-              <el-menu-item index="2-1">选项1</el-menu-item>
-              <el-menu-item index="2-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="2-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="2-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-            </el-submenu>
           </el-submenu>
           <el-submenu index="3">
             <template slot="title">
@@ -71,6 +60,7 @@
             </el-submenu>
           </el-submenu>
         </el-menu>
+        
       </el-aside>
       <el-main>
         <router-view />
