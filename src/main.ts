059d46c9a24ec2094b1879aae3fc10b7ffeb403d
libo <asd13148866@sina.com>
2019-01-17 17:04:43 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
 import './element/element'
-
 Vue.config.productionTip = false
 
 new Vue({
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // options...
+    baseUrl: "/vue-admin-page",
+    productionSourceMap: false
+
+}
